ddef572d32b257d4098d5bffe660c68726828608
Steven Hoffman <git@fustrate.com>
2018-03-10 23:01:27 -0800

    + teams
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/mlb_stats_api/teams.rb
+++ lib/mlb_stats_api/teams.rb
@@ -5,5 +5,23 @@ module MLBStatsAPI
     def team(team_id)
       get("/teams/#{team_id}")
     end
+
+    def affiliates(team_id, season: nil)
+      get("/teams/#{team_id}/affiliates", season: season)
+    end
+
+    def coaches(team_id, date: nil)
+      date ||= Date.today
+
+      get("/teams/#{team_id}/coaches", date: date.strftime('%m/%d/%Y'))
+    end
+
+    # def leaders(team_id)
+    #   get("/teams/#{team_id}/leaders")
+    # end
+
+    def roster(team_id, type:, date: nil)
+      get("/teams/#{team_id}/roster/#{type}", date: date.strftime('%m/%d/%Y'))
+    end
   end
 end
