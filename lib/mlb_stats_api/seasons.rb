5b85e2604ab242085cb2e3b87bd2ee44d9aaab55
Steven Hoffman <git@fustrate.com>
2018-03-10 23:01:02 -0800

    + seasons
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/mlb_stats_api/seasons.rb
+++ lib/mlb_stats_api/seasons.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
+# Although this group has two endpoints, they both have the same function.
+#
+# /v1/seasons/{seasonId} is the same as /v1/seasons?season={seasonId}
 module MLBStatsAPI
   module Seasons
+    # Fetch one or more seasons' start and end dates
+    #
+    # @param year [String] comma-separated list of years
+    #
+    # @return [Array<Hash>] a list of matching seasons
+    def seasons(year: nil)
+      get('/seasons', sportId: 1, season: year)
+    end
+    alias season seasons
   end
 end
