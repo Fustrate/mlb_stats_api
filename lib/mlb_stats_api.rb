a4ed13345d9050ffeb66ec65abdf6d313992b152
Steven Hoffman <git@fustrate.com>
2018-10-07 14:24:20 -0700

    no more Venue class
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/mlb_stats_api.rb
+++ lib/mlb_stats_api.rb
@@ -10,7 +10,6 @@ require 'mlb_stats_api/version'
 require 'mlb_stats_api/base'
 require 'mlb_stats_api/live_feed'
 require 'mlb_stats_api/team'
-require 'mlb_stats_api/venue'
 
 require 'mlb_stats_api/conferences'
 require 'mlb_stats_api/config'
